237202b7648ee0899c75e76517580aabd957500a
Beyers <nel.beyers@gmail.com>
2019-02-04 20:52:44 +0200

    Init

---
+++ public/component---src-pages-index-js-06ddb88f9fc39736c8c4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{133:function(n,e,o){"use strict";o.r(e);var t=o(0),l=o.n(t);e.default=function(){return l.a.createElement("div",null,"Hello world!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-06ddb88f9fc39736c8c4.js.map
